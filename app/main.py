c1efc24abb13280caba8d467b58044b79f119fbb
Samir Debnam <ksnt7dpsgr@privaterelay.appleid.com>
2026-07-24 15:22:10 +1200

    Add database/settings setup

--- app/main.py
+++ app/main.py
@@ -1,13 +1,12 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
+from app.core.config import settings
 
 
 app = FastAPI()
 
 @app.get('/')
 def read_root():
-    return {'message': 'Welcome to my portfolio E-Commerce API!'}
+    return {'message': 'Welcome to my portfolio E-Commerce API!', 'db_configured' : bool(settings.database_url)}
 
-@app.get('/items/{item_id}')
-def read_item(item_id:int):
-    return {'item_id': item_id}
+
+
