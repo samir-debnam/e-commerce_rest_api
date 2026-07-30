381e7060217f85433619359a0e0b7d4b1ec25eb8
Samir Debnam <ksnt7dpsgr@privaterelay.appleid.com>
2026-07-30 14:11:52 +1200

    Add Category model and product-category relationship

--- alembic/env.py
+++ alembic/env.py
@@ -8,6 +8,7 @@ from alembic import context
 from app.core.config import settings
 from app.core.database import Base
 from app.models.product import Product
+from app.models.category import Category
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
--- app/models/product.py
+++ app/models/product.py
@@ -1,4 +1,6 @@
-from sqlalchemy import Column, Integer, String, Float
+from sqlalchemy import Column, Integer, String, Float, ForeignKey
+from sqlalchemy.orm import relationship
+
 from app.core.database import Base
 
 class Product(Base):
@@ -9,6 +11,12 @@ class Product(Base):
     description = Column(String, nullable=True)
     price = Column(Float, nullable=False )
     stock = Column(Integer, nullable=False, default=0)
+    category_id = Column(Integer, ForeignKey('categories.id'), nullable=True)
+
+    category = relationship('Category', back_populates='products')
+
+
+
 
 
     
