061db72b394cf967fe514c1ae4ffcadaefd02377
Samir Debnam <ksnt7dpsgr@privaterelay.appleid.com>
2026-09-14 13:26:36 +1200

    update ConfigDict to avoid depreciation warning

--- app/core/config.py
+++ app/core/config.py
@@ -1,10 +1,10 @@
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
 
 class Settings(BaseSettings):
     database_url: str
     secret_key: str
 
-    class Config:
-        env_file = '.env'
+    model_config = SettingsConfigDict(env_file='.env')
 
 settings = Settings()  # type: ignore[call-arg]
